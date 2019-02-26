14bb43e9288e9db092d8e5a1918ec27550f1710c
pjward5656 <patrickjward@uky.edu>
2019-02-26 16:40:00 -0500

    Fixed spelling error

--- R/rate_ci.R
+++ R/rate_ci.R
@@ -4,17 +4,17 @@
 #' @param numerator The numerator of the rate.
 #' @param denominator The denominator of the rate.
 #' @param alpha The significance level for the confidence interval
-#' @param multplier The multiplier for the rate. Defaults to 100,000.
+#' @param multiplier The multiplier for the rate. Defaults to 100,000.
 #' @keywords Rates
 #' @export
 #' @examples
 #' rate_ci()
 
-rate_ci<-function(numerator, denominator, alpha, multplier=100000){
+rate_ci<-function(numerator, denominator, alpha, multiplier=100000){
   list(
-    (numerator/denominator)*multplier,
-    exp(log((numerator/denominator)*multplier)-(qnorm(alpha/2, lower.tail=FALSE)*(1/sqrt(numerator)))),
-    exp(log((numerator/denominator)*multplier)+(qnorm(alpha/2, lower.tail=FALSE)*(1/sqrt(numerator))))
+    (numerator/denominator)*multiplier,
+    exp(log((numerator/denominator)*multiplier)-(qnorm(alpha/2, lower.tail=FALSE)*(1/sqrt(numerator)))),
+    exp(log((numerator/denominator)*multiplier)+(qnorm(alpha/2, lower.tail=FALSE)*(1/sqrt(numerator))))
   )
 }
 
