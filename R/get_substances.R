b34165edfb17df9268d78e44c1ad56b5cb6b7ced
pjward5656 <patrickjward@uky.edu>
2019-02-27 12:09:56 -0500

    Drug identification

---
+++ R/get_substances.R
@@ -0,0 +1,30 @@
+#' Extract substance information from free-text information
+#'
+#' This function creates indicator variables for substances that are listed in free-text. Indicator variables for a
+#' list of drugs and other terms are created in the dataframe supplied to the function.
+#' @param data The dataframe containing the free text information.
+#' @param ... The free text fields you wish to scan for substances.
+#' @keywords Drugs, substances, free-text scan
+#' @export
+#' @examples
+#' get_substances(data=death_certs, lina, lineb, linec, lined, injurydescription, contributingcauses)
+
+get_substances<-function(data, ...){
+  fields<-dplyr::quos(...)
+  suppressMessages(data %>%
+                     dplyr::mutate(scan=paste(!!! fields, sep=" ")) %>%
+                     tidytext::unnest_tokens(word, scan) %>%
+                     dplyr::anti_join(stop_words)  %>%
+                     dplyr::filter(!stringr::str_detect(word, pattern = "[[:digit:]]"),
+                                   !stringr::str_detect(word, pattern = "\\b(.)\\b"),
+                                   word %in% drugs$Term) %>%
+                     dplyr::count(casenumber, word) %>%
+                     dplyr::left_join(drugs, by=c("word"="Term")) %>%
+                     dplyr::select(casenumber, Drug) %>%
+                     unique() %>%
+                     dplyr::mutate(indicator=1) %>%
+                     tidyr::spread(key=Drug, value=indicator) %>%
+                     purrr::map_df(replace_na, 0) %>%
+                     dplyr::right_join(data))
+}
+
