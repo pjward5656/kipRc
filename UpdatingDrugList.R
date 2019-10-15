38fc57fe22457507941c52659d9c13fe94ad61a3
pjward5656 <patrickjward@uky.edu>
2019-10-15 10:24:29 -0400

    UpdateDrugs

--- UpdatingDrugList.R
+++ UpdatingDrugList.R
@@ -1,6 +1,6 @@
 #Updating KIPRC Drug list
 
 library(readxl)
-data<-read_xlsx("X:/Vol 1/common/Patrick/Text scan/comb4.xlsx")
+drugs<-read_xlsx("X:/Vol 1/common/Patrick/Text scan/comb4.xlsx")
 
-save(data, file="C:/R Packages/kipRc/kipRc/data/drugs.Rda")
+save(drugs, file="C:/R Packages/kipRc/kipRc/data/drugs.Rda")
