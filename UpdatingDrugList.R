fd432efbbd4335e25b8f3f6e80bc305f60892c9b
pjward5656 <patrickjward@uky.edu>
2019-10-15 10:33:03 -0400

    FinalUpdate

--- UpdatingDrugList.R
+++ UpdatingDrugList.R
@@ -2,6 +2,7 @@
 
 library(readxl)
 drugs<-read_xlsx("X:/Vol 1/common/Patrick/Text scan/comb4.xlsx") %>%
-  rename(Term=Terms_found_on_death_certificate)
+  rename(Term=Terms_found_on_death_certificate) %>%
+  mutate(Term=tolower(Term))
 
 save(drugs, file="C:/R Packages/kipRc/kipRc/data/drugs.Rda")
